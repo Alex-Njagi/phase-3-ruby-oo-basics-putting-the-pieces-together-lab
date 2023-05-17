2c0c68ac0c8b3a1732020b896b24d67fdcd530bd
Alex Njagi <1992anjagi@gmail.com>
2023-05-17 14:08:35 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize (title)
+        @title = title
+        def author=(author)
+            @author=author
+        end
+        def pages=(pages)
+            @page_count=pages
+        end
+        def genre=(genre)
+            @genre=genre
+        end
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    #def properties (title, author, pages, genre)
+        #@title = title
+        #@author = author
+        #@pages = pages
+        #@genre = genre
+    #end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize (brand)
+        @brand = brand
+        def color=(color)
+            @color = color
+        end
+        def size=(size)
+            @size = size
+        end
+        def material=(material)
+            @material = material
+        end
+        def condition=(condition)
+            @condition = condition
+        end
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
